000067e89c11b9f89eef61c0cdab7f6914ebbfa9
BYUNSUJUNG <susususu07@naver.com>
2025-02-10 17:47:54 +0900

    전체적으로 CSS 업데이트

--- src/main/resources/static/js/main/style-category.js
+++
@@ -1,6 +0,0 @@
-const viewSizeSelect = document.getElementById('view-size-select');
-viewSizeSelect.onchange = () => {
-    const searchParams = new URLSearchParams(location.search);
-    searchParams.set('size', viewSizeSelect.value);
-    location.href = `/product?${searchParams.toString()}`;
-}
